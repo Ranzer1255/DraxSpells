81d8bb4b600fced40399d47dd3e4a47be5590ef9
Bobby <sgmaniac1255@yahoo.com>
2017-06-22 15:38:06 -0500

    create testing data and testing output

--- app/src/main/java/com/example/ranzer/draxspells/MainActivity.java
+++ app/src/main/java/com/example/ranzer/draxspells/MainActivity.java
@@ -2,6 +2,16 @@ package com.example.ranzer.draxspells;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.ranzer.draxspells.data.SimpleSpellDataProvider;
+import com.example.ranzer.draxspells.data.SpellItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+		List<String> spellList = new ArrayList<>();
+		for (SpellItem spell : SimpleSpellDataProvider.spellList) {
+			spellList.add(spell.getName());
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, spellList);
+
+		ListView lv = (ListView) findViewById(android.R.id.list);
+		lv.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/ranzer/draxspells/data/SimpleSpellDataProvider.java
+++ app/src/main/java/com/example/ranzer/draxspells/data/SimpleSpellDataProvider.java
@@ -1,13 +1,38 @@
 package com.example.ranzer.draxspells.data;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class SimpleSpellDataProvider {
 
-	public static List<SpellItem> SpellList;
-	public static Map<String, SpellItem> SpellMap;
+	public static List<SpellItem> spellList;
+	public static Map<String, SpellItem> spellMap;
 
+	static {
+		spellList = new ArrayList<>();
+		spellMap = new HashMap<>();
 
+		addSpell(null, "Fireball", 3, "envocation", "S,V,M", "a ball of fire", "Dex Save", null, "8d6");
+		addSpell(null, "Firebolt", 0, "envocation", "S,v", "a  small ball of fire", null, "dex", "1d10");
+	}
+
+	public static void addSpell(String spellID, String name, int level, String school, String components,
+								String description, String saveDC, String attackBonus, String damage) {
+		if (spellID != null) {
+			spellID = UUID.randomUUID().toString();
+		}
+		SpellItem spell = new SpellItem(spellID, name, level, school, components,
+				description, saveDC, attackBonus, damage);
+
+		addSpell(spell);
+	}
+
+	public static void addSpell(SpellItem spell) {
+		spellList.add(spell);
+		spellMap.put(spell.getSpellID(), spell);
+	}
 }
