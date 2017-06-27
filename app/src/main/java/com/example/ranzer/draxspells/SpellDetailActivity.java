0bf7059e0089cd1568c951ed49290afa01d86f90
Ranzer <sgmaniac1255@yahoo.com>
2017-06-27 13:51:07 -0500

    more work on spell detail screen

--- app/src/main/java/com/example/ranzer/draxspells/SpellDetailActivity.java
+++ app/src/main/java/com/example/ranzer/draxspells/SpellDetailActivity.java
@@ -7,9 +7,14 @@ import android.widget.TextView;
 import com.example.ranzer.draxspells.data.SimpleSpellDataProvider;
 import com.example.ranzer.draxspells.data.SpellItem;
 
+import org.w3c.dom.Text;
+
 public class SpellDetailActivity extends AppCompatActivity {
 
 	private SpellItem spellItem;
+	private TextView tvSpellName, tvLevelSchool, tvSpellDescription, tvCastingTime, tvRange;
+	private TextView tvComponents, tvDuration;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +28,9 @@ public class SpellDetailActivity extends AppCompatActivity {
 		int spellPos = getIntent().getIntExtra(MainActivity.SPELL_POSITION, -1);
 		spellItem = SimpleSpellDataProvider.spellList.get(spellPos);
 
-		TextView tvSpellName = (TextView) findViewById(R.id.spellName);
-		TextView tvLevelSchool = (TextView) findViewById(R.id.levelAndSchool);
-		TextView tvSpellDescription = (TextView) findViewById(R.id.spellDescription);
+		getViews();
 
-		tvSpellName.setText(spellItem.getName());
+		setSpellName();
 		StringBuilder sb = new StringBuilder();
 
 		if (spellItem.getLevel() == 0) {
@@ -37,6 +40,31 @@ public class SpellDetailActivity extends AppCompatActivity {
 		}
 
 		tvSpellDescription.setText(spellItem.getDescription());
+
+		tvCastingTime.setText(spellItem.getCastingTime());
+		tvRange.setText(spellItem.getRange());
+		tvComponents.setText(spellItem.getComponents());
+		tvDuration.setText(spellItem.getDuration());
+	}
+
+	private void setSpellName() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(spellItem.getName());
+		if (spellItem.isConcentration()) {
+			sb.append(" [C]");
+		}
+		tvSpellName.setText(sb.toString());
+	}
+
+	private void getViews() {
+		tvSpellName = (TextView) findViewById(R.id.spellName);
+		tvLevelSchool = (TextView) findViewById(R.id.levelAndSchool);
+		tvSpellDescription = (TextView) findViewById(R.id.spellDescription);
+		tvCastingTime = (TextView) findViewById(R.id.CastingTime);
+		tvRange = (TextView) findViewById(R.id.Range);
+		tvComponents = (TextView) findViewById(R.id.components);
+		tvDuration = (TextView) findViewById(R.id.duration);
 	}
 
 	public static String ordinal(int i) {
--- app/src/main/java/com/example/ranzer/draxspells/data/SimpleSpellDataProvider.java
+++ app/src/main/java/com/example/ranzer/draxspells/data/SimpleSpellDataProvider.java
@@ -21,10 +21,18 @@ public class SimpleSpellDataProvider {
 						"The fire spreads around corners. it ignites flammable objects in the area that aren't being worn or carried.",
 				"When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd",
 				"Dex Save", null, "8d6");
+
 		addSpell(null, "Firebolt", 0, "Evocation", "1 Action", "120 Feet", "V, S", "Instantaneous", false,
 				"You hurl a mote of fire at a creature or object within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 fire damage. a flammable object hit by this spe3ll ignites if it isn't being worn ro carried.",
 				"This spell's damage increases by 1d10 when you reach 5th level (2d10, 11th level (3d10), and 17h level (4d10)",
 				null, "1d20+Prof+Dex", "1d10");
+
+		addSpell(null, "Project Image", 7, "Illusion", "1 Action", "500 miles", "V,S,M", "Concentration, up to 1 day",
+				true, "You create an illusory copy of yourself that lasts for the duration, The copy can appear at any location within range that you have seen befor, regardless of intervening obstacles. The illusion looks and sounds like you but is intangible. If the isllusion takes any damage, it disappears, and the spell ends.\n" +
+						"you can use your action to move this illusion up to twice your speed, and make it gesture, speak and behave in whatever way you choose. It mimics your mannerisms perfectly.\n" +
+						"you can see through its eyes and hear through its ears as if you were in its space. On your turn as a bonus action, you can swithc from using its senses to using your own, or back again. While you are using its senses, you are blinded and deafend in regard to your own surroundinghs.\n" +
+						"Phisical interactions with the image reveals it to be an illusion, because things can bass through it. A creature that usis its action to examine the image can determine that it is an aillusion with a successful Inteligence (Investication_ check aginst your spell save DC. If a creature discerns the illlusion for what it is, the creature can see through the image, and any noise it makes sounds hollow tothe creature.",
+				null, null, null, null);
 	}
 
 	public static void addSpell(String spellID, String name, int level, String school, String castingTime,
